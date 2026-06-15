ffce5a93018e2599e77aacc4fb7ce62d54365874
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-06-15 01:50:38 -0300

    Automatically test AMI and CMI encodings

--- test_encodings.py
+++ test_encodings.py
@@ -1,14 +1,30 @@
+#!/bin/env python
 #!/bin/env python3
 
 import numpy as np
 
 import Encodings as enc
-import Dencodings as dec
+import Decodings as dec
 
 test_vect = np.array([1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1])
 
-def test_manchester():
-    t, encoded_manchester = enc.Manchester(test_vect)
-    decoded_manchester = dec.Manchester(encoded_manchester)
 
-    assert np.array_equal(test_vect, decoded_manchester)
+def test_AMI():
+    t, encoded = enc.AMI(test_vect)
+    t, decoded = dec.AMI(encoded)
+
+    assert np.array_equal(test_vect, decoded)
+
+
+def test_CMI():
+    t, encoded = enc.CMI(test_vect)
+    t, decoded = dec.CMI(encoded)
+
+    assert np.array_equal(test_vect, decoded)
+
+
+def test_manchester():
+    t, encoded = enc.Manchester(test_vect)
+    t, decoded = dec.Manchester(encoded)
+
+    assert np.array_equal(test_vect, decoded)
